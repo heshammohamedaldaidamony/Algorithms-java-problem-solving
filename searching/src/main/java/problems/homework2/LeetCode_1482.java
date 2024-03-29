8fd05f8ce9be4a4ba820f9a496c41d89be2f334a
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-03-29 21:13:41 +0200

    1482 my sol with a hint

---
+++ searching/src/main/java/problems/homework2/LeetCode_1482.java
@@ -0,0 +1,44 @@
+package problems.homework2;
+
+import java.util.Arrays;
+
+public class LeetCode_1482 {
+
+    public static boolean afterDAPocketFinished(int[] bloomDay, int m, int k,int d){
+            int count=0;
+            for(int i =0 ;i<bloomDay.length;i++) {
+                if (bloomDay[i] <= d){
+                    count++;
+                    if (count == k){
+                        m--;
+                        count=0;
+                    }
+                    if(m==0)
+                        return true;
+                }
+                else
+                    count=0;
+
+            }
+        return false;
+    }
+    public static int minDays(int[] bloomDay, int m, int k) {
+        int start=1;int end= Arrays.stream(bloomDay).max().getAsInt(); //n
+        int answer=-1;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if(afterDAPocketFinished(bloomDay,m,k,mid)){
+                end=mid-1;
+                answer=mid;
+            }
+            else
+                start=mid+1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int [] arr={1,10,3,10,2};
+        System.out.println(minDays(arr,3,1));
+    }
+}
