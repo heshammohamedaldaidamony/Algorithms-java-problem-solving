7b6c0486a3002fef760e27dd792293e257fe4536
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-03-29 14:08:41 +0200

    441 dr mostafa solution

--- searching/src/main/java/problems/homework1/LeetCode_441.java
+++ searching/src/main/java/problems/homework1/LeetCode_441.java
@@ -11,6 +11,24 @@ public class LeetCode_441 {
         }
         return rows;
     }
+    // log n - binary search generalization
+    public static boolean possible(int n , long rows){
+        long sum=(rows*(rows+1))/2;
+        return n>=sum;
+    }
+    public static int arrangeCoins_drSol(int n) {
+        int start=0;int end=n;int answer=0;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if(possible(n,mid)){
+                start=mid+1;
+                answer=mid;
+            }
+            else
+                end=mid-1;
+        }
+        return answer;
+    }
 
     public static void main(String[] args) {
         System.out.println(arrangeCoins(5));
