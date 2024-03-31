ca09e1f8365479a26874212d2d06c81a8d6eb840
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-03-31 14:48:08 +0200

    pie my sol

---
+++ searching/src/main/java/problems/homework4/Spoj_Pie.java
@@ -0,0 +1,37 @@
+package problems.homework4;
+
+public class Spoj_Pie {
+
+    public static boolean possible(double []pie , int people,double area){
+        int count=0;
+        for(int i =0 ;i <pie.length;i++){ //n (pie size) * n (pie[i] size)
+            double copy=pie[i];
+            while (copy>=area){
+                copy-=area;
+                count++;
+            }
+        }
+        return count>=people;
+    }
+    static double pie(double []pieArea , int people) {
+        double EPS = 1e-9;
+        double start = 0, end = Integer.MIN_VALUE;
+        for(int i =0 ; i <pieArea.length;i++){
+            end=Math.max(end,pieArea[i]);
+        }
+        while (end - start > EPS) {
+            double mid = start + (end - start) / 2;
+            if (possible(pieArea,people,mid))
+                start = mid;
+
+            else
+                end = mid;
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        double []pie={5,4,3,2.5};
+        System.out.println(pie(pie,6));
+    }
+}
