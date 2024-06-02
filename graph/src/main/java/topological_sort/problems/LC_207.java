7df269f0ff8cd54fb560f70fd24ed18f1bec77dc
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-06-02 18:43:08 +0300

    207 by topological sort algorithm

---
+++ graph/src/main/java/topological_sort/problems/LC_207.java
@@ -0,0 +1,44 @@
+package topological_sort.problems;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LC_207 {
+    List<Integer> [] graph  ;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        buildGraph(numCourses,prerequisites);
+        return topoSort(graph);
+    }
+    public void buildGraph(int numCourses, int[][] prerequisites){
+        graph= new ArrayList[numCourses];
+        for (int i =0 ; i<numCourses ; i++)
+            graph[i]=new ArrayList<>();
+        for (int[] i : prerequisites)
+            graph[i[1]].add(i[0]);
+    }
+    public boolean topoSort(List<Integer>[] graph){
+        int[] inDegree = new int[graph.length];
+        for (int i =0  ; i <graph.length ;i++)
+            for (int j : graph[i])
+                inDegree[j]++;
+
+        Queue<Integer> readyQueue = new LinkedList<>();
+        for (int i =0 ; i<inDegree.length ; i++)
+            if(inDegree[i]==0)
+                readyQueue.add(i);
+
+        List<Integer> result = new ArrayList<>();
+        while (!readyQueue.isEmpty()){
+            int current = readyQueue.remove();
+            result.add(current);
+            for (int i : graph[current])
+                if(--inDegree[i]==0)
+                    readyQueue.add(i);
+        }
+        if(result.size()!=graph.length)
+            return false;
+        return true;
+    }
+}
