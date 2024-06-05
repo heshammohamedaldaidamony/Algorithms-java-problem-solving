a8456fbbc6d3b1744cde7bec56f336bb5934023e
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-06-05 04:11:10 +0300

    310 dr sol

--- graph/src/main/java/topological_sort/problems/LC_310.java
+++ graph/src/main/java/topological_sort/problems/LC_310.java
@@ -4,56 +4,34 @@ import java.util.*;
 
 public class LC_310 {
     List<Integer>[] graph  ;
-    List<Integer>[] levelNodes;
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if(n==1)
-            return Arrays.asList(new Integer []{0});
-        List<Integer> result ;
         buildGraph(n,edges);
-        int maxLevel=bfs(n);
-        if(n>2)
-            maxLevel+=2;
-        result=levelNodes[maxLevel/2];
-        System.out.println(maxLevel);
-        if( maxLevel!=0 && maxLevel%2 ==0){  //special case
-            for(int i : levelNodes[maxLevel/2-1])
-                result.add(i);
-        }
-        return result;
+        return bfs(n);
     }
-    public  int bfs(int n) {
-        int []visited = new int[n];
-
+    public  List<Integer> bfs(int n) {
         int[] inDegree = new int[graph.length];  // to find the leaf
         for (int i =0  ; i <graph.length ;i++)
             for (int j : graph[i])
                 inDegree[j]++;
 
         Queue<Integer> readyQueue = new LinkedList<>();
-        for (int i =0 ; i<inDegree.length ; i++){
-            if(inDegree[i]==1){
+        for (int i =0 ; i<inDegree.length ; i++)
+            if(inDegree[i]==1)
                 readyQueue.add(i);
-                visited[i]=1;
-            }
-        }
-        levelNodes=new ArrayList[n];
-        for (int i =0 ; i<n ; i++)
-            levelNodes[i]=new ArrayList<>();
-
-        int level =0;
-        for (int size = readyQueue.size(); !readyQueue.isEmpty() ; size = readyQueue.size() , level++) {
+        List<Integer> result=new ArrayList<>();
+        result.add(0);   //special case n==1
+        for (int size = readyQueue.size(); !readyQueue.isEmpty(); size = readyQueue.size()) {
+            result =new ArrayList<>();
             while (size != 0) {
                 int current = readyQueue.remove();
-                levelNodes[level].add(current);
+                result.add(current);
                 for (int i : graph[current])
-                    if(visited[i]!=1){
+                    if(--inDegree[i]==1)
                         readyQueue.add(i);
-                        visited[i]=1;
-                    }
                 size--;
             }
         }
-        return level;
+        return result;
     }
     public void buildGraph(int n, int[][] edges){
         graph= new ArrayList[n];
