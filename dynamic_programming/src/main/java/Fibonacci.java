ccae06a28ed3391bdd191b2c0edea45c1ab922a0
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-06-09 00:37:11 +0300

    fibonacci bottom up approach and an enhancment for it

--- dynamic_programming/src/main/java/Fibonacci.java
+++ dynamic_programming/src/main/java/Fibonacci.java
@@ -11,6 +11,7 @@ public class Fibonacci {
     public void fillArray(){
         Arrays.fill(memory,-1);
     }
+    //top dowan
     public long fib_v2(int n){
         if(n<=1)
             return 1;
@@ -18,12 +19,35 @@ public class Fibonacci {
             return memory[n];
         return memory[n] = fib_v2(n-1)+fib_v2(n-2);
     }
+    //bottom up   o(n) time , memory - more efficient with stack(no auxiliary space)
+    public long fib_v3(int n){
+        long[] fib = new long[n+1];
+        fib[0]=fib[1]=1;  // base case
+        for (int i =2 ; i<fib.length ; i++)
+            fib[i]=fib[i-1] + fib[i-2];
+        return fib[n];
+    }
+    //bottom up enhanced  o(n) time , o(1)memory - more efficient with stack(no auxiliary space)
+    public long fib_v3_improved(int n){
+        long a=1;
+        long b=1;
+        long c=-1;
+        for (int i =2 ; i<=n ; i++){
+            c= a + b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+
 
     public static void main(String[] args) {
         Fibonacci fibonacci= new Fibonacci();
         //System.out.println(fibonacci.fib_v1(80));
         fibonacci.fillArray();
         System.out.println(fibonacci.fib_v2(80));
+        System.out.println(fibonacci.fib_v3(80));
+        System.out.println(fibonacci.fib_v3_improved(80));
     }
 
 }
