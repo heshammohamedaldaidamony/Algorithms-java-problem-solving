3f8a768411ef8c49ef7448e200794367c8dafd89
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-06-09 17:22:04 +0300

    enhace with some dp - just the defination - dont go to in valid path

--- dynamic_programming/src/main/java/Knapsack.java
+++ dynamic_programming/src/main/java/Knapsack.java
@@ -36,6 +36,25 @@ public class Knapsack {
 
         return Math.max(choice1,choice2);
     }
+    public int knapsack2(int[] weights,int[]values ,int maxSize , int remainingSize, int start){
+        if ( start==result.length){
+            int sumValues=0;
+            for (int i =0 ; i<result.length ; i++){
+                if(result[i]!=-1 )
+                    sumValues+=values[i];
+            }
+            return sumValues;
+        }
+        result[start]=-1;   // leave
+        int choice1=knapsack(weights, values,maxSize, start+1) ;
+
+        int choice2=0;
+        if (remainingSize <= weights[start]){
+            result[start]=weights[start];    // pick
+            choice2= knapsack(weights, values,maxSize, start+1);
+        }
+        return Math.max(choice1,choice2);
+    }
 
 
     public static void main(String[] args) {
@@ -46,7 +65,8 @@ public class Knapsack {
         int start=0;
         new Knapsack().fillArray();
         System.out.println(new Knapsack().knapsack(weights,values,maxSize,start));
-
+        new Knapsack().fillArray();
+        System.out.println(new Knapsack().knapsack2(weights,values,maxSize,maxSize,start));
 
 
     }
