98f5c89eff4a6019ee469886cfc30dae21920620
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-06-10 17:56:05 +0300

    lis dr sol - n^2

--- dynamic_programming/src/main/java/LIS.java
+++ dynamic_programming/src/main/java/LIS.java
@@ -29,9 +29,27 @@ public class LIS {
         return Math.max(choice1,choice2);
     }
 
+    int [][] memory = new int[50][50];
+    public int lis2(int [] sequence,int previous, int start){
+        if ( start==sequence.length)
+            return 0;
+        if(memory[start][previous]!=0)
+            return memory[start][previous];
+
+        int choice1= lis2(sequence,previous,start+1); //leave
+        int choice2=0;
+        if (previous>=sequence.length || sequence[start] >= sequence[previous] ){
+            choice2 = 1 + lis2(sequence, start,start+1);
+        }
+
+        memory[start][previous]=Math.max(choice1,choice2);
+        return memory[start][previous];
+    }
+
     public static void main(String[] args) {
         int [] array = {15,2,7,4,6,9};
         System.out.println(new LIS().lis(array,-1,0));
+        System.out.println(new LIS().lis2(array,array.length,0)); // -1 as the initial value for prev makes a problem . solve it flagging
 
 
     }
