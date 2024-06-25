a86405c125fa51c0fed6ef133834f072223b59c1
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-06-25 13:40:47 +0300

    198

---
+++ dynamic_programming/src/main/java/problems/LC_198.java
@@ -0,0 +1,26 @@
+package problems;
+
+public class LC_198 {
+    int [][] memo;
+    public int rob(int[] nums) {
+        memo=new int[nums.length][nums.length];
+        for (int i = 0 ; i<memo.length; i++)
+            for (int  j =0 ; j<memo[i].length;j++)
+                memo[i][j]=-1;
+        return dp(nums,0,-1);
+    }
+    public int dp(int [] nums , int idx, int prev){
+        if(idx==nums.length)
+            return 0;
+        if(prev!=-1 && memo[idx][prev]!=-1)
+            return memo[idx][prev];
+        int choice1=dp(nums,idx+1,prev);
+        int choice2=0;
+        if(prev==-1 || idx-prev>1)
+        choice2=nums[idx]+dp(nums,idx+1,idx);
+
+        if(prev!=-1)
+            memo[idx][prev]=Math.max(choice1,choice2);
+        return Math.max(choice1,choice2);
+    }
+}
