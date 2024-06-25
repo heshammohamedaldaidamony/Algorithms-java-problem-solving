021cb2d2b6096ab707f75d014c2ef40e6b95a4a1
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-06-25 08:31:09 +0300

    1671 - wrong implementation for lis and lds after dr sol point(looping)

--- dynamic_programming/src/main/java/problems/LC_1671.java
+++ dynamic_programming/src/main/java/problems/LC_1671.java
@@ -3,55 +3,77 @@ package problems;
 public class LC_1671 {
     int [][] memoryLIS ;
     int [][] memoryLDS ;
+    int startLIS;
+    int startLDS;
     public int minimumMountainRemovals(int[] nums) {
-        memoryLIS=new int[nums.length+1][nums.length+1];
-        memoryLDS=new int[nums.length+1][nums.length+1];
-        for(int i =0 ; i<memoryLIS.length;i++)
-            for (int j =0 ; j<memoryLIS[i].length ; j++)
-                memoryLIS[i][j]=memoryLDS[i][j]=-1;
+        int n = nums.length;
+        memoryLIS = new int[n + 1][n + 1];
+        memoryLDS = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
+                memoryLIS[i][j] = -1;
+                memoryLDS[i][j] = -1;
+            }
+        }
 
-        // depends on the mountain constraint . i start form up so why dont search for the start of up
-        int i=1 ;
-        for (; i<nums.length;i++)
-            if(nums[i]>nums[i-1])
-                break;
-        int lis =LIS(nums,nums.length,0);
-        int lds= LDS(nums,nums.length,0);
-        System.out.println(lis + " " + lds +" "+ nums.length);
-        return nums.length-(lis+lds-1);
+//        // an optimization
+//        // depends on the mountain constraint . i start form up so why dont search for the start of up
+//        int i=1 ;
+//        for (; i<nums.length;i++)
+//            if(nums[i]>nums[i-1])
+//                break;
+//        int j=nums.length-1 ;
+//        for (; j>0;j--)
+//            if(nums[j-1]>nums[j])
+//                break;
+//        startLIS=i-1;
+//        startLDS=j;
+
+        int longestDecreasing=0;
+        for (int l =0 ;l<nums.length;l++ ){
+            int lis=LIS(nums,nums.length,l);
+            int lds=LDS(nums,nums.length,l);
+            System.out.println(lis);
+            System.out.println(lds);
+            System.out.println();
+            if(lis==1 || lds ==1)
+                continue; // mountain must have 3+ values
+            longestDecreasing=Math.max(longestDecreasing,lis+lds-1);
+        }
+        return nums.length-longestDecreasing;
     }
 
     public int LIS(int [] sequence, int previous, int start){
-        if ( start==sequence.length)
+        if ( start==sequence.length )
             return 0;
         if(memoryLIS[start][previous]!=-1)
             return memoryLIS[start][previous];
 
         int choice1= LIS(sequence,previous,start+1); //leave
         int choice2=0;
-        if (previous>=sequence.length || sequence[start] > sequence[previous] )
+        if (previous==sequence.length || sequence[start] > sequence[previous] )
             choice2 = 1 + LIS(sequence, start,start+1);
 
         memoryLIS[start][previous]=Math.max(choice1,choice2);
         return memoryLIS[start][previous];
     }
-    public int LDS(int [] sequence, int previous, int start){
-        if ( start==sequence.length)
+    public int LDS(int [] sequence, int previous, int start ){
+        if ( start==sequence.length )
             return 0;
         if(memoryLDS[start][previous]!=-1)
             return memoryLDS[start][previous];
 
         int choice1= LDS(sequence,previous,start+1); //leave
         int choice2=0;
-        if (previous>=sequence.length || sequence[start] < sequence[previous] )
+        if (previous==sequence.length || sequence[start] < sequence[previous] )
             choice2 = 1 + LDS(sequence, start,start+1);
 
         memoryLDS[start][previous]=Math.max(choice1,choice2);
         return memoryLDS[start][previous];
     }
-    
+
     public static void main(String[] args) {
-        int[] nums={2,9,19,45,41,96,72,40,100,37,36,13,7};
+        int[] nums={4,3,2,1,1,2,3,1};
         System.out.println(new LC_1671().minimumMountainRemovals(nums));
     }
 }
