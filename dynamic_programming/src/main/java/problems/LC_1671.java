89059836dc60cb524dc14d959baf3bbd949acd10
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-06-25 09:02:08 +0300

    1671 customize lis and lds for looping

--- dynamic_programming/src/main/java/problems/LC_1671.java
+++ dynamic_programming/src/main/java/problems/LC_1671.java
@@ -1,18 +1,18 @@
 package problems;
 
 public class LC_1671 {
-    int [][] memoryLIS ;
-    int [][] memoryLDS ;
+    int [] memoryLIS ;
+    int [] memoryLDS ;
     int startLIS;
     int startLDS;
     public int minimumMountainRemovals(int[] nums) {
         int n = nums.length;
-        memoryLIS = new int[n + 1][n + 1];
-        memoryLDS = new int[n + 1][n + 1];
+        memoryLIS = new int[n + 1];
+        memoryLDS = new int[n + 1];
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= n; j++) {
-                memoryLIS[i][j] = -1;
-                memoryLDS[i][j] = -1;
+                memoryLIS[i] = -1;
+                memoryLDS[i] = -1;
             }
         }
 
@@ -31,11 +31,9 @@ public class LC_1671 {
 
         int longestDecreasing=0;
         for (int l =0 ;l<nums.length;l++ ){
-            int lis=LIS(nums,nums.length,l);
-            int lds=LDS(nums,nums.length,l);
-            System.out.println(lis);
-            System.out.println(lds);
-            System.out.println();
+            int lis=LIS(nums,l);
+            int lds=LDS(nums,l);
+
             if(lis==1 || lds ==1)
                 continue; // mountain must have 3+ values
             longestDecreasing=Math.max(longestDecreasing,lis+lds-1);
@@ -43,33 +41,33 @@ public class LC_1671 {
         return nums.length-longestDecreasing;
     }
 
-    public int LIS(int [] sequence, int previous, int start){
-        if ( start==sequence.length )
+    public int LIS(int [] sequence, int start){
+        if ( start==-1 )
             return 0;
-        if(memoryLIS[start][previous]!=-1)
-            return memoryLIS[start][previous];
+        if(memoryLIS[start]!=-1)
+            return memoryLIS[start];
 
-        int choice1= LIS(sequence,previous,start+1); //leave
-        int choice2=0;
-        if (previous==sequence.length || sequence[start] > sequence[previous] )
-            choice2 = 1 + LIS(sequence, start,start+1);
-
-        memoryLIS[start][previous]=Math.max(choice1,choice2);
-        return memoryLIS[start][previous];
+        memoryLIS[start]=0;  // to wont be corrupted with memoryLIS[start]++; if it equals -1
+        for (int prev =start-1 ; prev>=0; prev--){
+            if(sequence[prev] < sequence[start])
+                memoryLIS[start]=Math.max(memoryLIS[start],LIS(sequence,prev));
+        }
+        memoryLIS[start]++;  // this trick instead of 1+LIS to handle -1
+        return memoryLIS[start];
     }
-    public int LDS(int [] sequence, int previous, int start ){
+    public int LDS(int [] sequence, int start ){
         if ( start==sequence.length )
             return 0;
-        if(memoryLDS[start][previous]!=-1)
-            return memoryLDS[start][previous];
+        if(memoryLDS[start]!=-1)
+            return memoryLDS[start];
 
-        int choice1= LDS(sequence,previous,start+1); //leave
-        int choice2=0;
-        if (previous==sequence.length || sequence[start] < sequence[previous] )
-            choice2 = 1 + LDS(sequence, start,start+1);
-
-        memoryLDS[start][previous]=Math.max(choice1,choice2);
-        return memoryLDS[start][previous];
+        memoryLDS[start]=0;
+        for (int next =start+1 ; next<sequence.length ; next++){
+            if(sequence[next] < sequence[start])
+                memoryLDS[start]=Math.max(memoryLDS[start],LDS(sequence,next));
+        }
+        memoryLDS[start]++;
+        return memoryLDS[start];
     }
 
     public static void main(String[] args) {
