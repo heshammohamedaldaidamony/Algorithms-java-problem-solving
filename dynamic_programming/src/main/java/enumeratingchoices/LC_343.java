cfeaf06b2c392da4603056329abad7424fd67ac3
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-07-16 00:46:29 +0300

    lc 343

--- dynamic_programming/src/main/java/enumeratingchoices/LC_343.java
+++ dynamic_programming/src/main/java/enumeratingchoices/LC_343.java
@@ -1,2 +1,29 @@
-package enumeratingchoices;public class LC_343 {
+package enumeratingchoices;
+
+public class LC_343 {
+    int[] memo;
+    public int integerBreak(int n) {
+        if(n==2|| n==3)
+            return n-1;
+        memo=new int[n+1];
+        return productSum(n);
+    }
+
+    public int productSum(int n){
+        if(n==1)
+            return 1;
+
+        if(memo[n]!=0)
+            return memo[n];
+
+        int max=n;
+        for (int i =1 ; i< n ; i++)   //we considere the n in the previous line
+            max=Math.max(max,i * productSum(n-i));
+
+        memo[n]=max;
+        return max;
+    }
+    public static void main(String[] args) {
+        System.out.println(new LC_343().integerBreak(5));
+    }
 }
