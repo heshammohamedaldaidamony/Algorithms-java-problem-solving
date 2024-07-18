a832c4f111fe41f5759464be2f6b1f339a04089f
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-07-18 19:22:27 +0300

    1547 dr sol - loop on cuts instead of the stick itself

--- dynamic_programming/src/main/java/range/LC_1547.java
+++ dynamic_programming/src/main/java/range/LC_1547.java
@@ -1,20 +1,23 @@
 package range;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class LC_1547 {
-    int [] cuts;
+    List<Integer> cuts=new ArrayList<>();
     private int[][] memo;
 
     public int minCost(int n, int[] cuts) {
-        this.cuts=cuts;
-        memo=new int[n+1][n+1];
+        this.cuts.add(0);
+        this.cuts.add(n);
+        for (int i : cuts)
+            this.cuts.add(i);
+        Collections.sort(this.cuts);
+        memo=new int[this.cuts.size()][this.cuts.size()];
 
-        return dp(0,n);
-    }
-    public boolean validCutIdx(int cutIdx){
-        for (int i =0 ; i<cuts.length; i++)
-            if(cuts[i]==cutIdx)
-                return true;
-        return false;
+        return dp(0,this.cuts.size()-1);
     }
     public int dp(int start , int end){
         if(end == start+1)
@@ -23,18 +26,14 @@ public class LC_1547 {
             return memo[start][end];
 
         memo[start][end]=Integer.MAX_VALUE;
-        int cost= end-start;
-        for (int i =start+1  ; i<end ; i++){
-            if(!validCutIdx(i))
-                continue;
+        int cost= cuts.get(end)-cuts.get(start);
+        for (int i =start+1  ; i<end ; i++){  // loop on cuts list
             int left=dp(start,i);
             int right= dp(i,end);
             memo[start][end] = Math.min(memo[start][end] ,cost+left+right);
         }
-        System.out.println(memo[start][end]);
         return memo[start][end];
     }
-
     public static void main(String[] args) {
         int [] cus={1,3,4,5};
         System.out.println(new LC_1547().minCost(7,cus));
