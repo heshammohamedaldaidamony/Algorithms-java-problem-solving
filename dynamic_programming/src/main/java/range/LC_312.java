6e4a2a78ff2e903a0ea05b6fd5a4d9c2f32f7e7e
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-07-19 17:23:36 +0300

    312 cant handle memoization

--- dynamic_programming/src/main/java/range/LC_312.java
+++ dynamic_programming/src/main/java/range/LC_312.java
@@ -9,7 +9,7 @@ public class LC_312 {
         for (int i =0 ; i<memo.length ; i++)
             for (int j=0 ; j<memo.length; j++)
                 memo[i][j]=-1;
-        return dp();
+        return dp(0);  //0 index order
     }
     public int countValidCoins(int idx ){
         int coins=1*nums[idx];
@@ -27,17 +27,22 @@ public class LC_312 {
         }
         return coins;
     }
-    public int dp(){
+    public int dp( int order){
+        if(order==nums.length)
+            return 0;
         int maxCoins=0;
         for (int i =0 ; i<nums.length ; i++){
+            if (memo[i][order]!=-1)   // the bug is here 
+                return memo[i][order];
             if(nums[i]==-1)
                 continue;
             int myCoins=countValidCoins(i);   //handle the business logic
             int current=nums[i];
             nums[i]=-1;   // remark the ballon as bursted
-            maxCoins=Math.max(maxCoins,myCoins + dp());
+            maxCoins=Math.max(maxCoins,myCoins + dp(order+1));
             nums[i]=current;
-            
+
+            memo[i][order]=maxCoins;
         }
         return maxCoins;
     }
