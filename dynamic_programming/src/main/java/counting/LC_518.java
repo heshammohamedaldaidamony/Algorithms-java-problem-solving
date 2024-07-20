91114c1d75a70d1287aa4a3c3eef834463297217
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-07-21 00:04:00 +0300

    518 dr sol

--- dynamic_programming/src/main/java/counting/LC_518.java
+++ dynamic_programming/src/main/java/counting/LC_518.java
@@ -2,30 +2,32 @@ package counting;
 
 public class LC_518 {
     int[] coins;
-    boolean[] visited;
+    int[][] memo;
     public int change(int amount, int[] coins) {
         this.coins=coins;
-        this.visited=new boolean[amount+1];
+        this.memo=new int[coins.length][amount+1];
+        for (int i =0 ; i<memo.length ; i++)
+            for (int j=0 ; j<memo[i].length; j++)
+                memo[i][j]=-1;
 
-        return dp(amount,0);
+        return dp(0,amount);
     }
-    public int dp( int amount , int noCoins){
-        if (amount<0 || (amount==0 && visited[noCoins]))   // to handle counting repeats
+    public int dp(int idx, int amount ){
+        if(idx==coins.length || amount<0)
             return 0;
-        if(amount==0 ){
-            visited[noCoins]=true;
+        if(amount==0 )
             return 1;
-        }
+        if (memo[idx][amount]!=-1)
+            return memo[idx][amount];
+        int take =dp(idx,amount-coins[idx]);
+        int leave =dp(idx+1,amount);
 
-        int result=0;
-        for (int i =0 ; i<coins.length ; i++){
-            result+= dp(amount-coins[i],noCoins+1);
-        }
-        return result;
+        memo[idx][amount]=take+leave;
+        return take+leave;
     }
 
     public static void main(String[] args) {
         int[] coins={1,2,5};
-        System.out.println(new LC_518().change(20,coins));
+        System.out.println(new LC_518().change(500,coins));
     }
 }
