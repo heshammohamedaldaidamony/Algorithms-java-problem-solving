205f3271a27f6fc53c6a1cb1956dca1fbe28812e
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-07-30 01:26:19 +0300

    lc 516 - another approach - accepted

--- dynamic_programming/src/main/java/outcourseproblems/LC_516.java
+++ dynamic_programming/src/main/java/outcourseproblems/LC_516.java
@@ -1,62 +1,35 @@
 package outcourseproblems;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class LC_516 {
     String string;
-    HashMap<String,Integer> memo=new HashMap<>();
+    int [][] memo;
     public int longestPalindromeSubseq(String s) {
         string=s;
-        return dp(0,"");
+        memo=new int[s.length()][s.length()];
+        for (int[]arr:memo)
+            Arrays.fill(arr,-1);
+
+        return dp(0,s.length()-1);
     }
-    public static boolean isPalindrome(String str) {
-        if (str == null) {
-            return false;
-        }
-
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            // Move the left index to the next alphanumeric character
-            while (left < right && !Character.isLetterOrDigit(str.charAt(left))) {
-                left++;
-            }
-            // Move the right index to the previous alphanumeric character
-            while (left < right && !Character.isLetterOrDigit(str.charAt(right))) {
-                right--;
-            }
-            // Compare characters
-            if (Character.toLowerCase(str.charAt(left)) != Character.toLowerCase(str.charAt(right))) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
-    }
-    public int dp(int idx , String str){
-        if (idx==string.length() && isPalindrome(str))
+    public int dp(int start,int end){
+        if (start==end)
+            return 1;
+        if (start>end)
             return 0;
-        if (idx==string.length() )
-            return Integer.MIN_VALUE;
+        if (memo[start][end]!=-1)
+            return memo[start][end];
 
-        String key=String.valueOf(idx)+"|"+str;
-        if (memo.containsKey(key))
-            return memo.get(key);
-
-        String copy=str;
-        copy+=string.charAt(idx);
-        int pick=1+dp(idx+1,copy);
-        int leave=dp(idx+1,str);
-
-        memo.put(key,Math.max(pick,leave));
-        return memo.get(key);
+        if (string.charAt(start)==string.charAt(end))
+            return 2+dp(start+1,end-1);
+        memo[start][end]=Math.max(dp(start+1,end),dp(start,end-1));
+        return memo[start][end];
     }
 
     public static void main(String[] args) {
-        String s ="euazbipzncptldueeuechubrcourfpftcebikrxhybkymimgvldiwqvkszfycvqyvtiwfckexmowcxztkfyzqovbtmzpxojfofbvwnncajvrvdbvjhcrameamcfmcoxryjukhpljwszknhiypvyskmsujkuggpztltpgoczafmfelahqwjbhxtjmebnymdyxoeodqmvkxittxjnlltmoobsgzdfhismogqfpfhvqnxeuosjqqalvwhsidgiavcatjjgeztrjuoixxxoznklcxolgpuktirmduxdywwlbikaqkqajzbsjvdgjcnbtfksqhquiwnwflkldgdrqrnwmshdpykicozfowmumzeuznolmgjlltypyufpzjpuvucmesnnrwppheizkapovoloneaxpfinaontwtdqsdvzmqlgkdxlbeguackbdkftzbnynmcejtwudocemcfnuzbttcoew";
+        String s ="aaa";
         System.out.println(new LC_516().longestPalindromeSubseq(s));
     }
 
