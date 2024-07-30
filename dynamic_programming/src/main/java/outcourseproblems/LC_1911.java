2c2801341a2c4735d8e63aeb7766ac45e9aadaaa
20912020101106 <20912020101106@fci.zu.edu.eg>
2024-07-30 18:12:51 +0300

    lc 1911

--- dynamic_programming/src/main/java/outcourseproblems/LC_1911.java
+++ dynamic_programming/src/main/java/outcourseproblems/LC_1911.java
@@ -1,47 +1,41 @@
 package outcourseproblems;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
 public class LC_1911 {
     int[] nums;
-    HashMap<String,Integer> memo=new HashMap<>();
-    public long maxAlternatingSum(int[] nums) {
-        this.nums=nums;
-        return dp(0,new ArrayList<>());
-    }
-    public static int alternatingSum(List<Integer> nums) {
-        int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (i % 2 == 0) {
-                sum += nums.get(i);
-            } else {
-                sum -= nums.get(i);
-            }
-        }
-        return sum;
-    }
-    public int dp(int idx, List<Integer> list){
-        if (idx==nums.length)
-            return alternatingSum(list);
-        String key=String.valueOf(idx)+"|"+String.valueOf(list);
+    Map<String, Long> memo = new HashMap<>();
 
+    public long maxAlternatingSum(int[] nums) {
+        this.nums = nums;
+        return dp(0, true);
+    }
+
+    private long dp(int idx, boolean isEven) {
+        if (idx == nums.length)
+            return 0;
+
+        String key = idx + "|" + isEven;
         if (memo.containsKey(key))
             return memo.get(key);
 
-        List<Integer> copy=list;
-        copy.add(nums[idx]);
-        int pick=dp(idx+1,copy);
-        copy.removeLast();
-        int leave=dp(idx+1,list);
+        long pick = 0;
+        if (isEven)
+            pick = nums[idx] + dp(idx + 1, false);
+        else
+            pick = -nums[idx] + dp(idx + 1, true);
+        
 
-        memo.put(key,Math.max(pick,leave));
-        return memo.get(key);
+        long skip = dp(idx + 1, isEven);
+
+        long result = Math.max(pick, skip);
+        memo.put(key, result);
+        return result;
     }
 
     public static void main(String[] args) {
-        int[] nums={5,6,7,8};
-        System.out.println(new LC_1911().maxAlternatingSum(nums));
+        int[] nums = {374, 126, 84, 237, 195, 139, 328, 353, 286, 113, 351, 167, 394, 398, 29, 118, 17, 162, 206, 138, 34, 109, 291, 368, 162, 109, 336, 256, 203, 330, 235, 74, 136, 72, 127, 382, 288, 276, 135, 383, 300, 220, 299, 205, 186, 113, 71, 261, 253, 47, 387, 25, 57, 79, 322, 82, 349, 217, 306, 33, 198, 196, 306, 240, 271, 129, 284, 6, 349, 370, 59, 350, 275, 385, 137, 394, 329, 175, 58, 151, 223, 81, 233, 2, 370, 369, 135, 257, 391, 92, 260, 55, 321, 153, 328, 260, 312, 102, 79, 192};
+        System.out.println(new LC_1911().maxAlternatingSum(nums));  // Expected output should be the correct maximum alternating sum
     }
 }
